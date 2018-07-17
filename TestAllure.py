67e765b16a303d925645aab0979890aeb97473de
Ruber Cuellar <rubercuellar@hotmail.com>
2018-07-17 10:01:13 -0400

    Signed-off-by: Ruber Cuellar <rubercuellar@hotmail.com>

---
+++ TestAllure.py
@@ -0,0 +1,11 @@
+import pytest
+import allure
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("3*5", 15),
+    ("2*4", 8),
+    ("6*9", 54),
+])
+def test_eval(test_input, expected):
+    allure.attach(test_input)
+    assert eval(test_input) == expected
